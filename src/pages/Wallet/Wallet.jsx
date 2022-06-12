f9be04dd25b99d9396c3fbe60ce43639303cb33a
Berkay <berkaycirak_11@hotmail.com>
2022-06-12 16:53:01 +0300

    Wallet page is built

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -1,7 +1,7 @@
 import './Wallet.styles.scss';
 
 import { ethers } from 'ethers';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import ContractContext from '../../context/SmartContractActions';
 import { toast } from 'react-toastify';
 
@@ -11,7 +11,9 @@ function Wallet() {
   let signer;
   const [balance, setBalance] = useState(null);
   const [walletAddress, setWalletAddress] = useState('');
-  const { myContract } = useContext(ContractContext);
+  const [tokensOwner, setTokensOwner] = useState([]);
+  const [filteredTokens, setFilteredTokens] = useState([]);
+  const { myContract, items } = useContext(ContractContext);
 
   // This function helps us to connect metamask.
   const connectWalletHandler = async () => {
@@ -28,10 +30,29 @@ function Wallet() {
   };
 
   const getBalanceHandler = async () => {
-    const yourBalance = await myContract.balanceOf(walletAddress);
-    console.log(ethers.utils.formatEther(yourBalance).toString() / 1e-18);
+    let yourBalance = await myContract.balanceOf(walletAddress);
+    yourBalance = ethers.utils.formatEther(yourBalance).toString() / 1e-18;
+    const nftCollection = await myContract.tokensOfOwner(walletAddress);
+    setTokensOwner(nftCollection);
   };
 
+  useEffect(() => {
+    // This will return id of the each token. Since return value is a big Number, we should convert it to normal number
+
+    const nftArray = [];
+    tokensOwner.forEach((token) => {
+      let x = ethers.utils.formatEther(token).toString() / 1e-18;
+      x = x.toFixed(0);
+      nftArray.push(Number(x));
+    });
+
+    const filteredNFTs = items.filter((item) => nftArray.includes(item.id));
+    console.log('Filtered NFTs', filteredNFTs);
+
+    setFilteredTokens(filteredNFTs);
+  }, [tokensOwner, items]);
+
+  console.log(tokensOwner);
   return (
     <div className='wallet-container'>
       <div className='wallet'>
