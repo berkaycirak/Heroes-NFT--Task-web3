ea95c54bfb08ebe958e957bb0590782b8055da7a
Berkay <berkaycirak_11@hotmail.com>
2022-06-14 14:04:18 +0300

    Video background is fixed for mobile devices

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Explore() {
   return (
     <div className='hero'>
-      <video src={videoBg} autoPlay muted loop></video>
+      <video src={videoBg} autoPlay muted loop playsInline></video>
       <div className='hero-button btn'>
         <Link className='link' to='/cards'>
           EXPLORE
