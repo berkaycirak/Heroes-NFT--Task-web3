75aa7016243526697e475b797eee7cf20400f739
Berkay <berkaycirak_11@hotmail.com>
2022-06-15 03:53:18 +0300

    Lazy Loading is added to images

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -5,7 +5,7 @@ function CardItem({ image, name, rarity, id }) {
   return (
     <div className='cardItem'>
       <Link to={`/cards/${id}`} className='categoryListingLink'>
-        <img className='cardItem-image' src={image} alt='' />
+        <img className='cardItem-image' src={image} alt='' loading='lazy' />
       </Link>
     </div>
   );
